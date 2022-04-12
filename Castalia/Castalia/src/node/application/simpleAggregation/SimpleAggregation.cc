8c201b3b302bc7267dfbd0f741d7df9446d8f125
Benjamin Becker <benjamin.becker@kom.e-technik.tu-darmstadt.de>
2022-04-12 16:07:41 +0200

    addCastalia

---
+++ Castalia/Castalia/src/node/application/simpleAggregation/SimpleAggregation.cc
@@ -0,0 +1,78 @@
+/****************************************************************************
+ *  Copyright: National ICT Australia,  2007 - 2010                         *
+ *  Developed at the ATP lab, Networked Systems research theme              *
+ *  Author(s): Athanassios Boulis, Dimosthenis Pediaditakis                 *
+ *  This file is distributed under the terms in the attached LICENSE file.  *
+ *  If you do not find this file, copies can be found by writing to:        *
+ *                                                                          *
+ *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia             *
+ *      Attention:  License Inquiry.                                        *
+ *                                                                          *  
+ ****************************************************************************/
+
+#include "SimpleAggregation.h"
+
+Define_Module(SimpleAggregation);
+
+void SimpleAggregation::startup()
+{
+	maxTTD =  ((long long) ((long) par("maxTTD"))) * ((long long) 1000);
+	sendInterval = (double)par("sendInterval") / 1000;
+	packetSize = (int)par("packetSize");
+	aggregatedValue = 0.0;
+	waitingTimeForLowerLevelData = 0.0;
+	lastSensedValue = 0.0;
+	totalPackets = 0;
+	
+	stringstream strValue; strValue << SELF_NETWORK_ADDRESS; unsigned int intValue; strValue >> intValue; std::srand(intValue); 	
+	double randomTime = (static_cast <double> (rand()) / static_cast <double> (RAND_MAX)) * sendInterval + sendInterval/2;
+	setTimer(SEND_AGGREGATED_VALUE, randomTime);
+	
+	// seed for random number generator 
+}
+
+void SimpleAggregation::timerFiredCallback(int index)
+{
+	switch (index) {	
+		case SEND_AGGREGATED_VALUE:{
+			DeadlinePacket *newPacket = new DeadlinePacket("App generic packet", APPLICATION_PACKET);
+			newPacket->setSequenceNumber(totalPackets);
+			newPacket->setByteLength(packetSize);
+			long long fac = 1000000;
+			newPacket->setDeadline(maxTTD + (long long)(getClock().dbl() * fac));
+			
+			toNetworkLayer(newPacket, "sink");
+
+			totalPackets++;
+			double randomTime = (static_cast <double> (rand()) / static_cast <double> (RAND_MAX)) * sendInterval + sendInterval/2;
+			trace() << "application: toNetworkLayer; next in " << randomTime;
+			setTimer(SEND_AGGREGATED_VALUE, randomTime);
+			break;
+		}
+	}
+}
+
+void SimpleAggregation::fromNetworkLayer(ApplicationPacket * rcvPacket, 
+		const char *source, double rssi, double lqi)
+{
+	double theData = rcvPacket->getData();
+
+	// do the aggregation bit. For this example aggregation function is a simple max.
+	if (theData > aggregatedValue)
+		aggregatedValue = theData;
+	if (isSink)
+		trace() << "application: fromNetworkLayer from " << source << "received value " << theData;
+}
+
+void SimpleAggregation::handleSensorReading(SensorReadingMessage * rcvReading)
+{
+	string sensType(rcvReading->getSensorType());
+	double sensValue = rcvReading->getSensedValue();
+	lastSensedValue = sensValue;
+}
+
+void SimpleAggregation::handleNeworkControlMessage(cMessage * msg)
+{
+	trace() << "application: handleNeworkControlMessage " << msg->getKind();
+}
+
---
+++ Castalia/Castalia/src/node/communication/routing/PLR/PLR.h
@@ -0,0 +1,140 @@
+/*******************************************************************************
+ *  Copyright: National ICT Australia,  2007 - 2011                            *
+ *  Developed at the ATP lab, Networked Systems research theme                 *
+ *  Author(s): Athanassios Boulis, Yuriy Tselishchev                           *
+ *  This file is distributed under the terms in the attached LICENSE file.     *
+ *  If you do not find this file, copies can be found by writing to:           *
+ *                                                                             *
+ *      NICTA, Locked Bag 9013, Alexandria, NSW 1435, Australia                *
+ *      Attention:  License Inquiry.                                           *
+ *                                                                             *
+ *******************************************************************************/
+
+#ifndef _PLR_H_
+#define _PLR_H_
+
+#include <map>
+#include <set>
+#include "VirtualRouting.h"
+#include "PLRPacket_m.h"
+#include "PLRControl_m.h"
+#include "CDF.h"
+
+#define NO_LEVEL  -110
+#define NO_SINK   -120
+
+using namespace std;
+
+enum PLRTimers {
+	TOPOLOGY_SETUP_TIMEOUT = 1,
+};
+
+struct cmp_str
+{
+   bool operator()(char const *a, char const *b) const
+   {
+      return strcmp(a, b) < 0;
+   }
+};
+
+class PLR: public VirtualRouting {
+ private:
+ 	// configurable
+	int pDFSlots;
+	long long maxDelay;
+	int pDFMessageTimes;
+	bool useAverageDelay;
+	bool useAvgSecond;
+	double probeInterval;
+	double cdfBroadcastInterval;
+	double newRoundInterval;
+	bool collectReducedTraceInfo;
+	
+	int appMaxTTD;
+	int appSendInterval;
+	
+	
+	// PLR-related member variables
+	int packetSize;
+	long long delayStep;
+	bool isSink;		//is a .ned file parameter of the Application module
+	std::set<int> neighbors;
+	//plr
+	std::map<int, double*> neighbor_cdfs;
+	std::map<int, double*> neighbor_pdfs;
+	std::map<int, double*> neighbor_pdfs_fixed;
+	std::map<int, double*> neighbor_hop_cdfs;
+	std::map<int, int*> neighbor_histograms;
+	int* routingTable;
+	int* routingTable_calc;
+	double* cdf;
+	//avg
+	std::map<int, double> neighbor_avgNodeDelays;
+	std::map<int, double> neighbor_avgLinkDelays;
+	std::map<int, double> neighbor_avgLinkDelays_fixed;
+	std::map<int, double> neighbor_avgHopDelays;
+	std::map<int, double> neighbor_avgLinkDelaysCounter;
+	int nextHop;
+	int nextHop_calc;
+	double avgDelay;
+	
+	//both
+	std::map<int, std::set<int>> seenPackets;
+	bool betterCDF;
+	
+	//rounds
+	int currentRound;
+	std::map<int, double> currentRound_neighbor_avgLinkDelays;
+	std::map<int, double*> currentRound_neighbor_pdfs;
+	
+
+	bool noTiming; 
+	// if true, then ...
+	// ... after 20 minutes: no more probes
+	// ... after 30 minutes: no more new rounds rounds
+	// ... after 40 minutes: no more broadcasts
+
+	// for monitoring
+	double simTime;
+	int monitoring_receivedPacketsInTime;
+	int monitoring_slots_1;
+	double monitoring_slots;
+	std::map<int, int*> monitoring_forwardedTo;
+	std::map<int, int*> monitoring_receivedFrom;
+	std::map<int, int*> monitoring_probeTo;
+	std::map<int, int*> monitoring_probeFrom;
+	std::map<int, int*> monitoring_SourceOfsuccessfullyDeliveredPackets;
+	int* monitoring_receivedCDFs;
+	int* monitoring_broadcastedCDFs;
+	int* monitoring_receivedProbes;
+	int* monitoring_sentProbes;
+	int* monitoring_receivedData;
+	int* monitoring_sentData;
+	int* monitoring_droppedData;
+	
+
+ protected:
+ 	void insertDelay(long long, int);
+ 	void updateAverageLinkDelay(double, int);
+ 	void createPDF(int);
+ 	void convoluteCDF(int);
+ 	void addMaxToCDF();
+ 	int getNextHop(long long);
+ 	long long toNanoseconds(double);
+ 	
+ 	void nextRound();
+ 	void broadcastCDF();
+ 	void sendProbe(int);
+
+	void startup();
+	void finish();
+	void fromApplicationLayer(cPacket *, const char *);
+	void fromMacLayer(cPacket *, int, double, double);
+	void handleNetworkControlCommand(cMessage *);
+	void sendControlMessage(PLRControlDef);
+
+	void timerFiredCallback(int);
+	void processBufferedPacket();
+};
+
+#endif				//PLRMODULE
